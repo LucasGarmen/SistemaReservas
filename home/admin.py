9e00e0d8c805592cdcadc700ebad04ade90f49d2
LucasGarmen <lucasgarmendia95@gmail.com>
2025-12-28 09:03:19 -0300

    agregando admin y superuser teste

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Habitacion, Cliente, Reserva
 
-# Register your models here.
+@admin.register(Habitacion)
+class HabitacionAdmin(admin.ModelAdmin):
+    list_display = ('numero', 'tipo', 'capacidad', 'valor_diaria', 'disponible')
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'email', 'telefono')
+
+@admin.register(Reserva)
+class ReservaAdmin(admin.ModelAdmin):
+    list_display = ('cliente', 'habitacion', 'fecha_entrada', 'fecha_salida', 'estado', 'total')
